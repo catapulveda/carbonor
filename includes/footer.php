a59c5cac68abbf6564034b5c0d1c62e71ae9d1e0
Nelson Castiblanco <catapulveda@hotmail.com>
2025-09-06 15:41:49 -0500

    Configuracion Inicial

---
+++ includes/footer.php
@@ -0,0 +1,108 @@
+<div class="container th-container2">
+    <div class="widget-area bg-black2">
+        <div class="footer-container">
+            <div class="row justify-content-between">
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget footer-widget">
+                        <div class="th-widget-about">
+                            <div class="about-logo">
+                                <a href="index.html"><img src="assets/img/logo-white.svg" alt="Builda"></a>
+                            </div>
+                            <p class="about-text"> Rapidiously myocardinate cross-platform intellectual capital model. Appropriately create interactive infrastructures</p>
+                            <div class="th-social style5">
+                                <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+                                <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                                <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget footer-widget">
+                        <h3 class="widget_title">Get In Touch</h3>
+                        <div class="th-widget-contact">
+                            <div class="info-box_text">
+                                <div class="icon"><img src="assets/img/icon/location-dot.svg" alt="img"></div>
+                                <div class="details">
+                                    <p>789 Inner Lane, Holy park,</p>
+                                    <p>California, USA</p>
+                                </div>
+                            </div>
+                            <div class="info-box_text">
+                                <div class="icon">
+                                    <img src="assets/img/icon/phone.svg" alt="img">
+                                </div>
+                                <div class="details">
+                                    <p><a href="tel:+0123456789" class="info-box_link">+01 234 567 890</a></p>
+                                    <p><a href="tel:+09876543210" class="info-box_link">+09 876 543 210</a></p>
+                                </div>
+                            </div>
+                            <div class="info-box_text">
+                                <div class="icon">
+                                    <img src="assets/img/icon/envelope.svg" alt="img">
+                                </div>
+                                <div class="details">
+                                    <p><a href="mailto:mailinfo00@builda.com" class="info-box_link">mailinfo00@builda.com</a></p>
+                                    <p><a href="mailto:support24@builda.com" class="info-box_link">support24@builda.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget widget_nav_menu footer-widget">
+                        <h3 class="widget_title">Useful Link</h3>
+                        <div class="menu-all-pages-container">
+                            <ul class="menu">
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="service.html">Featured Properties</a></li>
+                                <li><a href="service.html">Our Best Services</a></li>
+                                <li><a href="contact.html">Request Visit</a></li>
+                                <li><a href="contact.html">FAQ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-auto">
+                    <div class="widget widget_nav_menu footer-widget">
+                        <h3 class="widget_title">Explore</h3>
+                        <div class="menu-all-pages-container">
+                            <ul class="menu">
+                                <li><a href="service.html">All Properties</a></li>
+                                <li><a href="team.html">Our Agents</a></li>
+                                <li><a href="service.html">All Projects</a></li>
+                                <li><a href="about.html">Our Process</a></li>
+                                <li><a href="contact.html">Neighborhood</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="copyright-area" data-bg-src="assets/img/bg/footer-bg-3-1.png">
+    <div class="container th-container2">
+        <div class="copyright-wrap bg-white">
+            <div class="footer-container">
+                <div class="row gy-2 align-items-center">
+                    <div class="col-lg-6">
+                        <p class="copyright-text">
+                            Copyright <i class="fal fa-copyright"></i> 2025 <a href="index.html">Builda</a>, All rights reserved.</p>
+                    </div>
+                    <div class="col-lg-6 text-center text-lg-end">
+                        <div class="footer-links">
+                            <ul>
+                                <li><a href="contact.html">Terms of service</a></li>
+                                <li><a href="contact.html">Privacy policy</a></li>
+                                <li><a href="contact.html">Cookies</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/preloader.php
@@ -0,0 +1,10 @@
+<button class="th-btn preloaderCls">Cancel Preloader </button>
+<div class="preloader-inner">
+    <div class="mb-4">
+        <img src="assets/img/logo-icon.svg" alt="img">
+    </div>
+    <span class="loader">
+        BUILDA
+        <span class="loading-text">BUILDA</span>
+    </span>
+</div>
