b1a98af5fc3478220a3b24d5357d32822ff0c82e
Nelson Castiblanco <catapulveda@hotmail.com>
2025-09-29 13:57:41 -0500

    Footer translate

---
+++ assets/lang/br/footer.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    // About Section
+    'about_text' => 'Carbones Norte-Santandereanos S.A.S. – <strong>Carbonor</strong>, líder no setor mineral do Norte de Santander, Colômbia. Nos destacamos por qualidade, compromisso e responsabilidade com o meio ambiente.',
+    
+    // Contact Section
+    'contact_title' => 'Entre em contato',
+    'contact_location_1' => 'Cúcuta, Norte de Santander',
+    'contact_location_2' => 'Colômbia',
+    
+    // Useful Links Section
+    'useful_links_title' => 'Links Úteis',
+    'link_about' => 'Quem Somos',
+    'link_services' => 'Serviços',
+    'link_mining' => 'Mineração',
+    'link_contact' => 'Contato',
+    
+    // Explore Section
+    'explore_title' => 'Explorar',
+    'link_location' => 'Localização',
+    
+    // Copyright
+    'copyright_text' => 'Copyright <i class="fal fa-copyright"></i> 2025 <a href="https://ccnasys.com">ccnasys.com</a> Todos os direitos reservados.'
+];
